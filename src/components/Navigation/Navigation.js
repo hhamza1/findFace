5a13f21755d8e0d0dd868f09356e939b4f39f8ba
unknown <hamzahmitou@gmail.com>
2019-12-26 17:04:02 +0300

    Added the navigation container

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+
+const Navigation = () => {
+    return (
+            <nav>
+                <p className="f3 link dim black underline pa3 pointer">Sign out!</p>
+            </nav>
+    )
+}
+
+export default Navigation;
