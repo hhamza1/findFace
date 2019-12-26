38a7408bf3d6a0ed4326d31c407d9287949de174
unknown <hamzahmitou@gmail.com>
2019-12-26 17:02:46 +0300

    Added the app container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './App.css';
+import Navigation from './Navigation/Navigation';
+import Logo from './Logo/Logo';
+import ImgForm from './ImgForm/ImgForm';
+import Rank from './Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesParams = {
+    "particles": {
+        "number": {
+            "value": 160,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "line_linked": {
+            "enable": true
+        },
+    }
+}
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="App">
+        <Particles
+            params={particlesParams}
+            className="particles"
+        />
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImgForm />
         </div>
     )
 }
