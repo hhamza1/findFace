175c574fa845039dcf0946bc9fb17536efcc280e
unknown <hamzahmitou@gmail.com>
2019-12-30 17:17:34 +0300

    Implemented the face recognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import BoundingBox from '../../styled-components/BoundingBox';
 import './FaceRecognition.css';
 
 
-const FaceRecognition = ({imgUrl}) => {
+const FaceRecognition = ({imgUrl, box}) => {
+    console.log(box);
     return (
-        <div className="center ma">
+        <div className="ma center">
             <div className="absolute mt2">
-                <img className="inputImage" id="inputImg" src={imgUrl} alt="detectable faces" />
+                <img id="inputImg" src={imgUrl} alt="detectable faces" width="500px" height="auto" />
+                <BoundingBox 
+                    top={box.topRow} 
+                    right={box.rightCol} 
+                    bottom={box.bottomRow} 
+                    left={box.leftCol}
+                   />
             </div>
         </div>
     );
