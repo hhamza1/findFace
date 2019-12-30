d418f18674a1f0fbcb3ab47f3a1201d8c62316ff
unknown <hamzahmitou@gmail.com>
2019-12-30 11:06:00 +0300

    Passing prop methods

--- src/components/ImgForm/ImgForm.js
+++ src/components/ImgForm/ImgForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImgForm.css';
 
 
-const ImgForm = () => {
+const ImgForm = ({onInputChange, onDetect}) => {
     return (
         <div>
             <p className="f3">
@@ -10,8 +10,8 @@ const ImgForm = () => {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input className="f4 pa2 w-70 bw0 br3 center" type="text" />
-                    <button className="w-30 grow f4 bw0 br3 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                    <input className="f4 pa2 w-70 bw0 br3 center" type="text" onChange={onInputChange} />
+                    <button onClick={onDetect} className="w-30 grow f4 bw0 br3 link ph3 pv2 dib white bg-light-purple">Detect</button>
                 </div> 
             </div>
         </div>
